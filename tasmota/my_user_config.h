2ba7ca7c7d812867cfc661ea23f5cc64379030bf
Timo Willms <twillms@gs-networks.de>
2020-08-30 14:38:34 +0200

    set to standard

--- tasmota/my_user_config.h
+++ tasmota/my_user_config.h
@@ -50,9 +50,9 @@
 #define PROJECT                "Huehnerklappe"         // PROJECT is used as the default topic delimiter
 
 // If not selected the default will be SONOFF_BASIC
-#define MODULE                 HUEHNERKLAPPE_ST      // [Module] Select default module from tasmota_template.h
+#define MODULE                 SONOFF_BASIC      // [Module] Select default module from tasmota_template.h
 #ifdef ESP8266
-#define FALLBACK_MODULE        HUEHNERKLAPPE_ST      // [Module2] Select default module on fast reboot where USER_MODULE is user template
+#define FALLBACK_MODULE        SONOFF_BASIC      // [Module2] Select default module on fast reboot where USER_MODULE is user template
 //#define USER_TEMPLATE "{\"NAME\":\"Generic\",\"GPIO\":[255,255,255,255,255,255,255,255,255,255,255,255,255],\"FLAG\":15,\"BASE\":18}"  // [Template] Set JSON template
 #else // ESP32
 #define FALLBACK_MODULE        WEMOS             // [Module2] Select default module on fast reboot where USER_MODULE is user template
